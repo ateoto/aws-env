b576a11ccbd92f0729b00a546a7075f7e8beab36
Matt McCants <matt@moduscreate.com>
2014-10-29 14:15:34 -0400

    Initial commit.

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+	"github.com/vaughan0/go-ini"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "aws-env"
+	app.Usage = "Set AWS EnvVars from .aws/config file"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "profile, p",
+			Value: "default",
+			Usage: "Specify a profile to use",
+		},
+		cli.StringFlag{
+			Name:  "config, c",
+			Value: filepath.Join(os.Getenv("HOME"), ".aws", "config"),
+			Usage: "Specify a config file to use",
+		},
+	}
+
+	app.Action = func(c *cli.Context) {
+		profileKey := c.String("profile")
+
+		if c.String("profile") != "default" {
+			profileKey = fmt.Sprintf("profile %s", c.String("profile"))
+		}
+
+		file, err := ini.LoadFile(c.String("config"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for key, value := range file[profileKey] {
+			err := os.Setenv(strings.ToUpper(key), value)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("export %s=%s\n", strings.ToUpper(key), value)
+		}
+	}
+
+	app.Run(os.Args)
+}
